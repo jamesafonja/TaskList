be22808c5878cefcdffd0e1a2aeb5623770c1280
Soji <82482639+zero2coder@users.noreply.github.com>
2022-01-05 21:15:25 -0600

    Initial Commit

---
+++ TaskList/TaskListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskListApp.swift
+//  TaskList
+//
+//  Created by Revive on 1/5/22.
+//
+
+import SwiftUI
+
+@main
+struct TaskListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
